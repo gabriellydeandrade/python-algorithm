407da0e9610eaa2409448a4c0219bb2c4b05b501
gabriellydeandrade <gabrielly.andrade@poli.ufrj.br>
2020-05-14 11:58:42 -0300

    chore: adding second version of maximum pairwise product. It was necessary to use loops for the activity, so I remove max Python builtin

--- Algorithmic Toolbox/Maximum Pairwise Product/maximum_pairwise_product.py
+++ Algorithmic Toolbox/Maximum Pairwise Product/maximum_pairwise_product.py
@@ -1,17 +1,30 @@
-def get_maximum_pairwise_product(numbers: list) -> int:
+def get_maximum_pairwise_product(elements: int, numbers: list) -> int:
     """
     Maximum Pairwise Product Problem
     Find the maximum product of two distinct numbers in a sequence of non-negative integers.
     Input: A sequence of non-negative integers.
     Output: The maximum value that can be obtained by multiplying two different elements from the sequence.
     """
-    first_maximum_value = max(numbers)
+
+    # Second Version of Solution: using order 2n (removing max builtin from Python)
+    # TODO as a challenge, reduce the comparision for 1.5n and n + log(n) − 2
+
+    first_maximum_value = 0
+    for i in range(elements):
+        if numbers[i] > first_maximum_value:
+            first_maximum_value = numbers[i]
+
     numbers.remove(first_maximum_value)
-    second_maximum_value = max(numbers)
+
+    second_maximum_value = 0
+    for i in range(elements-1):
+        if numbers[i] > second_maximum_value:
+            second_maximum_value = numbers[i]
 
     return first_maximum_value * second_maximum_value
 
 
 if __name__ == "__main__":
-    user_input = [int(x) for x in input().split()]
-    print(get_maximum_pairwise_product(numbers=user_input))
+    user_number_of_elements = int(input())
+    user_numbers = [int(x) for x in input().split()]
+    print(get_maximum_pairwise_product(elements=user_number_of_elements, numbers=user_numbers))
