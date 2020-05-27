c4994e6e47e608a2f90578346b61cc160fb4f386
gabriellydeandrade <gabrielly.andrade@poli.ufrj.br>
2020-05-27 10:24:16 -0300

    :rotating_light: Test are not passing after I change the structure of decorator

--- Algorithmic Toolbox/greatest_common_divisor/validator.py
+++ Algorithmic Toolbox/greatest_common_divisor/validator.py
@@ -1,14 +1,14 @@
 def validate_args(func):
-    def param_is_integer(a, b):
+    def args_should_be_integer(a, b):
         if isinstance(a, int) and isinstance(b, int):
-            param_is_positive(a, b)
+            args_should_be_positive(a, b)
         else:
-            raise TypeError("Arguments passed should be integers to calculate")
+            raise TypeError("Arguments passed should be integers")
 
-    def param_is_positive(a, b):
+    def args_should_be_positive(a, b):
         if a >= 0 and b >= 0:
             return func(a, b)
         else:
-            raise AttributeError("Arguments passed shout be positive integers")
+            raise AttributeError("Arguments passed should be positive integers")
 
-    return param_is_integer
+    return args_should_be_integer
